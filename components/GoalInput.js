1d1a22813a1b4ace19c989588a49bda01789612b
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-12 00:21:52 -0500

    Clear the goal once added

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -8,6 +8,11 @@ const GoalInput = props => {
         setEnteredGoal(enteredText);
     }
 
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal)
+        setEnteredGoal('')
+    }
+
     return (
         <Modal visible={props.visible} animationType="slide">
             <View style={styles.inputContainer}>
@@ -17,7 +22,7 @@ const GoalInput = props => {
                     onChangeText={goalInputHandler}
                     value={enteredGoal}
                 />
-                <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+                <Button title="ADD" onPress={addGoalHandler} />
             </View>
         </Modal>
     )
