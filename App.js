63d030fdd05076a08fe0080985fe2231648d0bed
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-11 22:32:14 -0500

    Make the goal list scrollable

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -28,13 +28,13 @@ export default function App() {
           onPress={addGoalHandler}
         />
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map((goal) => (
           <View style={styles.listItem} key={goal}>
             <Text>{goal}</Text>
           </View>
         ))}
-      </View>
+      </ScrollView>
     </View >
   );
 }
