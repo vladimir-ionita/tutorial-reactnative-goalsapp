d2aca128aedee5f40d5017bf2103565f75d1f3ae
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-11 22:42:42 -0500

    Make the goal list a FlatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -28,13 +28,14 @@ export default function App() {
           onPress={addGoalHandler}
         />
       </View>
-      <ScrollView>
-        {courseGoals.map((goal) => (
-          <View style={styles.listItem} key={goal}>
-            <Text>{goal}</Text>
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
           </View>
-        ))}
-      </ScrollView>
+        )}
+      />
     </View >
   );
 }
